24c4731fc6ecc5f90475f2ee134479672a158f02
Amann1074 <160119769+Amann1074@users.noreply.github.com>
2025-03-08 00:20:25 +0530

    Create Square.java
    similarly,
    it extends shape,
    declares a private double variable,
    accepts a variable for input
    function to calculate area
    function to calculate perimeter

---
+++ Square.java
@@ -0,0 +1,19 @@
+// Square.java
+class Square extends Shape {
+    private double side;
+
+    Square(double side) {
+        super("Square");
+        this.side = side;
+    }
+
+    @Override
+    double calculateArea() {
+        return side * side;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 4 * side;
+    }
+}
