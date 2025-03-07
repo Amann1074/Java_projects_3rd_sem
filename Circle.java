6f3a3d18f88ee1a27fb68bfa2ef5cc468592d154
Amann1074 <160119769+Amann1074@users.noreply.github.com>
2025-03-08 00:11:23 +0530

    Create Circle.java
    Creates circle class that extends shape
    declares private double variable radius.
    function for calculating area
    function for calculating perimeter

---
+++ Circle.java
@@ -0,0 +1,21 @@
+
+import java.lang.Math;
+
+class Circle extends Shape {
+    private double radius;
+
+    Circle(double radius) {
+        super("Circle");
+        this.radius = radius;
+    }
+
+    @Override
+    double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
