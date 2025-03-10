ec1c3268384dfc7c5a7a8600197a5ed1f5dace92
Amann1074 <160119769+Amann1074@users.noreply.github.com>
2025-03-11 02:12:23 +0530

    Create SwimWithLegs.java

---
+++ SwimWithLegs.java
@@ -0,0 +1,5 @@
+public class SwimWithLegs implements SwimBehaviour { @Override
+public void swim() {
+System.out.println("I believe I can swim");
+}
+                                                   }
