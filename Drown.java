1134e632591e64cc30f9dfa9af28e6f67569f873
Amann1074 <160119769+Amann1074@users.noreply.github.com>
2025-03-11 02:06:41 +0530

    Create Drown.java

---
+++ Drown.java
@@ -0,0 +1,6 @@
+class Pyramid extends Shape implements Volume{
+public Pyramid(double length, double width,double height){ super(length,width,height,5);
+public class Drown implements SwimBehaviour { @Override
+public void swim() { System.out.println("Drowning!!!!");
+}
+}
